3d359d95ac5c811db9462820188d2f6198f785f2
Drew <drewedgette@gmail.com>
2024-11-30 17:34:16 -0500

    finished blog

--- personal/settings.py
+++ personal/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0wb3*h1n&$4or^ofymj0&s-6jejem__i42_j2&m_(@-kl+jrj'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["drewedgette.dev", "www.drewedgette.dev"]
 
 # Application definition
 
