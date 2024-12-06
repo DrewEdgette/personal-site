d3c322f6a1e8294876311f31b9aa712211df64ff
Drew <drewedgette@gmail.com>
2024-12-06 13:16:41 -0500

    update settings

--- personal/settings.py
+++ personal/settings.py
@@ -109,6 +109,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]  # Global static files
 STATIC_ROOT = BASE_DIR / "staticfiles"  # For production
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
